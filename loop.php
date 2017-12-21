d76799b1ddbff9d7593f5e85abea21a7efd324be
ldp <ldp@ldp.com>
2017-12-21 14:56:50 +0800

    ...

--- loop.php
+++ loop.php
@@ -9,7 +9,7 @@ $paged = (get_query_var( 'paged')) ? get_query_var('paged') : 1;
 $wp_query = new WP_Query('posts_per_page=5&paged='.$paged);
 while($wp_query->have_posts()):$wp_query->the_post();
 ?>
-<a href=""> <?php the_title(); ?></a>
+<a href=""> <?php the_ID(); ?></a>
 <?php endwhile;
 
 $wp_query =  null;
