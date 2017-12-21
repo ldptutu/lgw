8703e13697783ca35e550dcdc809120073259dc4
ldp <253496582@qq.com>
2017-12-21 09:17:46 +0800

    ...

--- category.php
+++ category.php
@@ -2,12 +2,14 @@
 get_header();
 ?>
 <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-8">
+    <div class="col-xs-12 col-sm-6 col-md-4">
 	<?php get_template_part( 'template-parts/navigation/navigation', 'left' ); ?>
-    </div>
-    <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-</div>
 
+    </div>
+    <div class="col-xs-6 col-md-8">
+	.col-xs-6 .col-md-4
+    </div>
+</div>
 
 <?php
 get_footer()
--- template-parts/navigation/navigation-left.php
+++ template-parts/navigation/navigation-left.php
@@ -1,12 +1,4 @@
 <?php
-
 create_left_menu('top');
-/*wp_nav_menu(array(
-    'theme_location' => 'top',
-    'menu_class'        => 'nav nav-custom',
-    'before' => '<span>',
-    'after' => '</span>',
-));
-*/
 ?>
 
