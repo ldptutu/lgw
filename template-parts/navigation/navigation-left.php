ebf0f0184d8ca3f6fed9b433fc02e3132130d93e
ldp <253496582@qq.com>
2017-12-21 08:35:57 +0800

    ...

--- template-parts/navigation/navigation-left.php
+++ template-parts/navigation/navigation-left.php
@@ -1,9 +1,14 @@
 <?php
-wp_nav_menu(array(
+
+
+clean_custom_menus();
+
+/*wp_nav_menu(array(
     'theme_location' => 'top',
     'menu_class'        => 'nav nav-custom',
     'before' => '<span>',
     'after' => '</span>',
 ));
+*/
 ?>
 
