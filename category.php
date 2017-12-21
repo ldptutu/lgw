970e51025c8684f80b12f0986466ffdf164cd37e
ldp <253496582@qq.com>
2017-12-21 09:18:43 +0800

    ..

--- category.php
+++ category.php
@@ -2,11 +2,11 @@
 get_header();
 ?>
 <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-4">
+    <div class="col-xs-12 col-sm-6 col-md-3">
 	<?php get_template_part( 'template-parts/navigation/navigation', 'left' ); ?>
 
     </div>
-    <div class="col-xs-6 col-md-8">
+    <div class="col-xs-6 col-md-9">
 	.col-xs-6 .col-md-4
     </div>
 </div>
