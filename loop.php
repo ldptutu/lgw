ac3c98ae07ba87b7c7e50e60b52feecd38fc0ba8
ldp <ldp@ldp.com>
2017-12-21 15:06:59 +0800

    ...

--- loop.php
+++ loop.php
@@ -3,9 +3,7 @@
 //do_action( 'storefront_loop_before' );
 
 $temp = $wp_query;
-$wp_query = null;
-//$paged = (get_query_var( 'paged')) ? get_query_var('paged') : 1;
-$paged = 1;
+$paged = (get_query_var( 'paged') != null) ? get_query_var('paged') : 1;
 
 $wp_query = new WP_Query('posts_per_page=5&paged='.$paged);
 while($wp_query->have_posts()):$wp_query->the_post();
