cb0c0f46547f626b66623ba4384379c98d085eb1
ldp <ldp@ldp.com>
2017-12-22 14:01:16 +0800

    ...

--- functions.php
+++ functions.php
@@ -134,7 +134,7 @@ if ( ! function_exists( 'lgw_post_meta' ) ) {
      *
      * @since 1.0.0
      */
-    function storefront_post_meta() {
+    function lgw_post_meta() {
 ?>
     <aside class="entry-meta">
 	<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search.
