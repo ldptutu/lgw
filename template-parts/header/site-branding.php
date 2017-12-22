e1a334929aa2c14397c005e6ee0c9b22b8939ad8
ldp <253496582@qq.com>
2017-12-23 07:38:25 +0800

    ....

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -4,7 +4,7 @@
  */
 ?>
 <div class="site-title">
-    <a  href="<?php echo esc_url(home_url('/')); ?>" style="padding-left:15px;">
+    <a  href="<?php echo esc_url(home_url('/')); ?>">
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" width="145" height="32" />
     </a>
     <p ><?php bloginfo('name'); ?></p>
