2cff41c620f826e4bfeec3a4a55926fe0eb12e13
ldp <253496582@qq.com>
2017-12-23 07:04:35 +0800

    ...

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -1,7 +1,6 @@
 <?php
-
 ?>
-<div class="navbar-header">
+<div class="navbar-header" style="float:none;margin-top:10px;">
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 	<span class="sr-only">Toggle navigation</span>
 	<span class="icon-bar"></span>
@@ -10,3 +9,4 @@
     </button>
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 </div>
+    
