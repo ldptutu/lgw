7ce9cba13210d5fefa4fa8ba6d5c496434df3618
ldp <253496582@qq.com>
2017-12-23 06:53:36 +0800

    ...

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -3,6 +3,8 @@
    Display header site branding
  */
 ?>
-<a class="navbar-left" href="<?php echo esc_url(home_url('/')); ?>">
-    <?php bloginfo('name'); ?>
-</a>
+<div class="navbar-left">
+    <a  href="<?php echo esc_url(home_url('/')); ?>">
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" width="145" height="32" />
+    </a>
+</div>
