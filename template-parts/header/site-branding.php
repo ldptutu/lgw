2871bb19c62c446385673cab24db2ff173fa2bb7
ldp <253496582@qq.com>
2017-12-23 07:06:13 +0800

    ...

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -3,7 +3,7 @@
    Display header site branding
  */
 ?>
-<a class="navbar-left"  href="<?php echo esc_url(home_url('/')); ?>">
+<a class="navbar-left"  href="<?php echo esc_url(home_url('/')); ?>" style="padding-left:15px;">
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" width="145" height="32" />
     </a>
 
