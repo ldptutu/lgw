5eac44b797816653003c00edd8e3700534215ec1
ldp <253496582@qq.com>
2017-12-23 07:29:08 +0800

    .....

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -7,7 +7,7 @@
 <a  href="<?php echo esc_url(home_url('/')); ?>" style="padding-left:15px;">
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" width="145" height="32" />
 </a>
-<span ><?php blog_info('name'); ?></span>
+<span ><?php bloginfo('name'); ?></span>
 </div>
 
 <div class="search-pannel">
