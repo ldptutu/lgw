df9faef1c30f4a85715f14075b067afadf4fb65d
ldp <253496582@qq.com>
2017-12-23 06:56:13 +0800

    ...

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -3,8 +3,7 @@
    Display header site branding
  */
 ?>
-<div class="navbar-left">
-    <a  href="<?php echo esc_url(home_url('/')); ?>">
+<a class="navbar-left"  href="<?php echo esc_url(home_url('/')); ?>">
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" width="145" height="32" />
     </a>
-</div>
+
