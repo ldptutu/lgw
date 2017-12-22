42765c970e576e0adb442ef9959dce393eeca956
ldp <ldp@ldp.com>
2017-12-22 13:58:33 +0800

    ...

--- functions.php
+++ functions.php
@@ -90,6 +90,103 @@ if (! function_exists( 'lgw_post_header' ) ) {
 <?php }
 }
 
+if ( ! function_exists( 'lgw_post_content' ) ) {
+    /**
+     * Display the post content with a link to the single post
+     *
+     * @since 1.0.0
+     */
+    function lgw_post_content() {
+?>
+    <div class="entry-content">
+	<?php
+
+	/**
+	 * Functions hooked in to storefront_post_content_before action.
+	 *
+	 * @hooked storefront_post_thumbnail - 10
+	 */
+	do_action( 'storefront_post_content_before' );
+
+	the_content(
+	    sprintf(
+		__( 'Continue reading %s', 'storefront' ),
+		'<span class="screen-reader-text">' . get_the_title() . '</span>'
+	    )
+	);
+
+	do_action( 'storefront_post_content_after' );
+
+	wp_link_pages( array(
+	    'before' => '<div class="page-links">' . __( 'Pages:', 'storefront' ),
+	    'after'  => '</div>',
+	) );
+	?>
+    </div><!-- .entry-content -->
+<?php
+}
+}
+
+
+if ( ! function_exists( 'lgw_post_meta' ) ) {
+    /**
+     * Display the post meta
+     *
+     * @since 1.0.0
+     */
+    function storefront_post_meta() {
+?>
+    <aside class="entry-meta">
+	<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search.
+
+	?>
+	    <div class="author">
+		<?php
+		echo get_avatar( get_the_author_meta( 'ID' ), 128 );
+		echo '<div class="label">' . esc_attr( __( 'Written by', 'storefront' ) ) . '</div>';
+		the_author_posts_link();
+		?>
+	    </div>
+	    <?php
+	    /* translators: used between list items, there is a space after the comma */
+	    $categories_list = get_the_category_list( __( ', ', 'storefront' ) );
+
+	    if ( $categories_list ) : ?>
+		<div class="cat-links">
+		    <?php
+		    echo '<div class="label">' . esc_attr( __( 'Posted in', 'storefront' ) ) . '</div>';
+		    echo wp_kses_post( $categories_list );
+		    ?>
+		</div>
+	    <?php endif; // End if categories. ?>
+
+	    <?php
+	    /* translators: used between list items, there is a space after the comma */
+	    $tags_list = get_the_tag_list( '', __( ', ', 'storefront' ) );
+
+	    if ( $tags_list ) : ?>
+		<div class="tags-links">
+		    <?php
+		    echo '<div class="label">' . esc_attr( __( 'Tagged', 'storefront' ) ) . '</div>';
+		    echo wp_kses_post( $tags_list );
+		    ?>
+		</div>
+	    <?php endif; // End if $tags_list. ?>
+
+	<?php endif; // End if 'post' == get_post_type(). ?>
+
+	<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+	    <div class="comments-link">
+		<?php echo '<div class="label">' . esc_attr( __( 'Comments', 'storefront' ) ) . '</div>'; ?>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'storefront' ), __( '1 Comment', 'storefront' ), __( '% Comments', 'storefront' ) ); ?></span>
+	    </div>
+	<?php endif; ?>
+    </aside>
+<?php
+}
+}
+
+
 
 
 
@@ -106,12 +203,12 @@ function create_left_menu( $theme_location ) {
     if ( ($theme_location) && ($locations = get_nav_menu_locations()) && isset($locations[$theme_location]) ) {
         $menu = get_term( $locations[$theme_location], 'nav_menu' );
         $menu_items = wp_get_nav_menu_items($menu->term_id);
- 
+	
         $menu_list = '<div class="list-group">' ."\n";
-          
+        
         foreach( $menu_items as $menu_item ) {
             if( $menu_item->menu_item_parent == 0 ) {
-                 
+                
                 $parent = $menu_item->ID;
 
                 $menu_list .= '<div class="list-group-item">' ."\n";
@@ -135,38 +232,38 @@ function create_left_menu( $theme_location ) {
         }
 
         $menu_list .= '</div><!-- /.container-fluid -->' ."\n";
-  
+	
     } else {
         $menu_list = '<!-- no menu defined in location "'.$theme_location.'" -->';
     }
-     
+    
     echo $menu_list;
 }
 
 function clean_custom_menus() {
-	$menu_name = 'top'; // specify custom menu slug
-	if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
-		$menu = wp_get_nav_menu_object($locations[$menu_name]);
-		$menu_items = wp_get_nav_menu_items($menu->term_id);
+    $menu_name = 'top'; // specify custom menu slug
+    if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
+	$menu = wp_get_nav_menu_object($locations[$menu_name]);
+	$menu_items = wp_get_nav_menu_items($menu->term_id);
         
-		$menu_list = '<nav>' ."\n";
-		$menu_list .= "\t\t\t\t". '<ul>' ."\n";
+	$menu_list = '<nav>' ."\n";
+	$menu_list .= "\t\t\t\t". '<ul>' ."\n";
 
 
         print_r($menu_list);
         
         
-		foreach ((array) $menu_items as $key => $menu_item) {
-			$title = $menu_item->title;
-			$url = $menu_item->url;
-			$menu_list .= "\t\t\t\t\t". '<li><a href="'. $url .'">'. $title .'</a></li>' ."\n";
-		}
-		$menu_list .= "\t\t\t\t". '</ul>' ."\n";
-		$menu_list .= "\t\t\t". '</nav>' ."\n";
-	} else {
-		// $menu_list = '<!-- no list defined -->';
+	foreach ((array) $menu_items as $key => $menu_item) {
+	    $title = $menu_item->title;
+	    $url = $menu_item->url;
+	    $menu_list .= "\t\t\t\t\t". '<li><a href="'. $url .'">'. $title .'</a></li>' ."\n";
 	}
-	echo $menu_list;
+	$menu_list .= "\t\t\t\t". '</ul>' ."\n";
+	$menu_list .= "\t\t\t". '</nav>' ."\n";
+    } else {
+	// $menu_list = '<!-- no list defined -->';
+    }
+    echo $menu_list;
 }
 
 function lgw_setup() {
@@ -210,64 +307,64 @@ add_action('after_setup_theme','lgw_setup');
 
 function my_attachments( $attachments )
 {
-  $fields         = array(
-    array(
-      'name'      => 'title',                         // unique field name
-      'type'      => 'text',                          // registered field type
-      'label'     => __( 'Title', 'attachments' ),    // label to display
-      'default'   => 'title',                         // default value upon selection
-    ),
-    
-    array(
-      'name'      => 'caption',                       // unique field name
-      'type'      => 'textarea',                      // registered field type
-      'label'     => __( 'Caption', 'attachments' ),  // label to display
-      'default'   => 'caption',                       // default value upon selection
-    ),
-  );
+    $fields         = array(
+	array(
+	    'name'      => 'title',                         // unique field name
+	    'type'      => 'text',                          // registered field type
+	    'label'     => __( 'Title', 'attachments' ),    // label to display
+	    'default'   => 'title',                         // default value upon selection
+	),
+	
+	array(
+	    'name'      => 'caption',                       // unique field name
+	    'type'      => 'textarea',                      // registered field type
+	    'label'     => __( 'Caption', 'attachments' ),  // label to display
+	    'default'   => 'caption',                       // default value upon selection
+	),
+    );
 
-  $args = array(
+    $args = array(
 
-    // title of the meta box (string)
-    'label'         => 'My Attachments',
+	// title of the meta box (string)
+	'label'         => 'My Attachments',
 
-    // all post types to utilize (string|array)
-    'post_type'     => array( 'post', 'page' ),
+	// all post types to utilize (string|array)
+	'post_type'     => array( 'post', 'page' ),
 
-    // meta box position (string) (normal, side or advanced)
-    'position'      => 'normal',
+	// meta box position (string) (normal, side or advanced)
+	'position'      => 'normal',
 
-    // meta box priority (string) (high, default, low, core)
-    'priority'      => 'high',
+	// meta box priority (string) (high, default, low, core)
+	'priority'      => 'high',
 
-    // allowed file type(s) (array) (image|video|text|audio|application)
-    'filetype'      => null,  // no filetype limit
+	// allowed file type(s) (array) (image|video|text|audio|application)
+	'filetype'      => null,  // no filetype limit
 
-    // include a note within the meta box (string)
-    'note'          => 'Attach files here!',
+	// include a note within the meta box (string)
+	'note'          => 'Attach files here!',
 
-    // by default new Attachments will be appended to the list
-    // but you can have then prepend if you set this to false
-    'append'        => true,
+	// by default new Attachments will be appended to the list
+	// but you can have then prepend if you set this to false
+	'append'        => true,
 
-    // text for 'Attach' button in meta box (string)
-    'button_text'   => __( 'Attach Files', 'attachments' ),
+	// text for 'Attach' button in meta box (string)
+	'button_text'   => __( 'Attach Files', 'attachments' ),
 
-    // text for modal 'Attach' button (string)
-    'modal_text'    => __( 'Attach', 'attachments' ),
+	// text for modal 'Attach' button (string)
+	'modal_text'    => __( 'Attach', 'attachments' ),
 
-    // which tab should be the default in the modal (string) (browse|upload)
-    'router'        => 'browse',
+	// which tab should be the default in the modal (string) (browse|upload)
+	'router'        => 'browse',
 
-    // whether Attachments should set 'Uploaded to' (if not already set)
+	// whether Attachments should set 'Uploaded to' (if not already set)
 	'post_parent'   => false,
 
-    // fields array
-    'fields'        => $fields,
+	// fields array
+	'fields'        => $fields,
 
-  );
+    );
 
-  $attachments->register( 'my_attachments', $args ); // unique instance name
+    $attachments->register( 'my_attachments', $args ); // unique instance name
 }
 
 add_action( 'attachments_register', 'my_attachments' );
@@ -278,21 +375,21 @@ add_filter( 'attachments_default_instance', '__return_false' ); // disable the d
 function lgw_color_css_wrap() {
     
 }
-    function lgw_scripts() {
-	    
-	    //bootstrap
-	    wp_enqueue_style('lgw-bootstrap',get_theme_file_uri('/assets/css/bootstrap.min.css'));
-        wp_enqueue_style('lgw-bootstrap',get_theme_file_uri('/assets/css/navbar-fixed-side.css'));
-	    
-	    wp_enqueue_style('lgw-style',get_stylesheet_uri());
-	    
-	    wp_enqueue_script('lgw-bootstrap',get_theme_file_uri('/assets/js/bootstrap.min.js'),array('jquery'),'3.3.7');
-	    
-	    //    wp_style_add_data( 'twentyseventeen-ie8', 'conditional', 'lt IE 9' );
-	    // Load the html5 shiv.
-	    //    wp_enqueue_script( 'html5', get_theme_file_uri( '/assets/js/html5.js' ), array(), '3.7.3' );
-	    //    wp_script_add_data( 'html5', 'conditional', 'lt IE 9' );
-	}
+function lgw_scripts() {
+    
+    //bootstrap
+    wp_enqueue_style('lgw-bootstrap',get_theme_file_uri('/assets/css/bootstrap.min.css'));
+    wp_enqueue_style('lgw-bootstrap',get_theme_file_uri('/assets/css/navbar-fixed-side.css'));
+    
+    wp_enqueue_style('lgw-style',get_stylesheet_uri());
+    
+    wp_enqueue_script('lgw-bootstrap',get_theme_file_uri('/assets/js/bootstrap.min.js'),array('jquery'),'3.3.7');
+    
+    //    wp_style_add_data( 'twentyseventeen-ie8', 'conditional', 'lt IE 9' );
+    // Load the html5 shiv.
+    //    wp_enqueue_script( 'html5', get_theme_file_uri( '/assets/js/html5.js' ), array(), '3.7.3' );
+    //    wp_script_add_data( 'html5', 'conditional', 'lt IE 9' );
+}
 add_action( 'wp_enqueue_scripts', 'lgw_scripts' );
 
 
@@ -315,9 +412,9 @@ add_action( 'lgw_single_post',         'lgw_post_meta',            20 );
 add_action( 'lgw_single_post',         'lgw_post_content',         30 );
 
 /*
-add_action( 'lgw_single_post_bottom',  'lgw_post_nav',             10 );
-add_action( 'lgw_single_post_bottom',  'lgw_display_comments',     20 );
-add_action( 'lgw_post_content_before', 'lgw_post_thumbnail',       10 );
-		*/
-		
+   add_action( 'lgw_single_post_bottom',  'lgw_post_nav',             10 );
+   add_action( 'lgw_single_post_bottom',  'lgw_display_comments',     20 );
+   add_action( 'lgw_post_content_before', 'lgw_post_thumbnail',       10 );
+ */
+
 require_once get_template_directory() . '/inc/wp-bootstrap-navwalker.php';
