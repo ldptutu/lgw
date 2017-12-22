2e66c0633abfea24a16b31b96a8c4b8cbe4c9759
ldp <253496582@qq.com>
2017-12-23 07:23:28 +0800

    ...

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -1,12 +1,14 @@
 <?php
 ?>
-<div class="navbar-header" style="float:none;margin-top:10px;">
+<div class="navbar-header" style="float:none;margin-top:20px;">
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 	<span class="sr-only">Toggle navigation</span>
 	<span class="icon-bar"></span>
 	<span class="icon-bar"></span>
 	<span class="icon-bar"></span>
     </button>
+    
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
+    
 </div>
     
--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -5,5 +5,9 @@
 ?>
 <a class="navbar-left"  href="<?php echo esc_url(home_url('/')); ?>" style="padding-left:15px;">
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" width="145" height="32" />
-    </a>
+</a>
+<span class="navbar-left"><?php blog_info('name'); ?></span>
+
+
+
 
