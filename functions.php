3338d501db198dd6f4e38866866ad845f54b32c4
ldp <ldp@ldp.com>
2017-12-22 13:51:14 +0800

    ...

--- functions.php
+++ functions.php
@@ -91,100 +91,6 @@ if ( ! function_exists( 'lgw_post_header' ) ) {
 	}
 }
 
-if ( ! function_exists( 'lgw_post_content' ) ) {
-	/**
-	 * Display the post content with a link to the single post
-	 *
-	 * @since 1.0.0
-	 */
-	function lgw_post_content() {
-		?>
-		<div class="entry-content">
-		<?php
-
-		/**
-		 * Functions hooked in to storefront_post_content_before action.
-		 *
-		 * @hooked storefront_post_thumbnail - 10
-		 */
-		do_action( 'storefront_post_content_before' );
-
-		the_content(
-			sprintf(
-				__( 'Continue reading %s', 'storefront' ),
-				'<span class="screen-reader-text">' . get_the_title() . '</span>'
-			)
-		);
-
-		do_action( 'storefront_post_content_after' );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'storefront' ),
-			'after'  => '</div>',
-		) );
-		?>
-		</div><!-- .entry-content -->
-		<?php
-	}
-}
-
-if ( ! function_exists( 'lgw_post_meta' ) ) {
-	/**
-	 * Display the post meta
-	 *
-	 * @since 1.0.0
-	 */
-	function storefront_post_meta() {
-		?>
-		<aside class="entry-meta">
-			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search.
-
-			?>
-			<div class="author">
-				<?php
-					echo get_avatar( get_the_author_meta( 'ID' ), 128 );
-					echo '<div class="label">' . esc_attr( __( 'Written by', 'storefront' ) ) . '</div>';
-					the_author_posts_link();
-				?>
-			</div>
-			<?php
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( __( ', ', 'storefront' ) );
-
-			if ( $categories_list ) : ?>
-				<div class="cat-links">
-					<?php
-					echo '<div class="label">' . esc_attr( __( 'Posted in', 'storefront' ) ) . '</div>';
-					echo wp_kses_post( $categories_list );
-					?>
-				</div>
-			<?php endif; // End if categories. ?>
-
-			<?php
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', __( ', ', 'storefront' ) );
-
-			if ( $tags_list ) : ?>
-				<div class="tags-links">
-					<?php
-					echo '<div class="label">' . esc_attr( __( 'Tagged', 'storefront' ) ) . '</div>';
-					echo wp_kses_post( $tags_list );
-					?>
-				</div>
-			<?php endif; // End if $tags_list. ?>
-
-		<?php endif; // End if 'post' == get_post_type(). ?>
-
-			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-				<div class="comments-link">
-					<?php echo '<div class="label">' . esc_attr( __( 'Comments', 'storefront' ) ) . '</div>'; ?>
-					<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'storefront' ), __( '1 Comment', 'storefront' ), __( '% Comments', 'storefront' ) ); ?></span>
-				</div>
-			<?php endif; ?>
-		</aside>
-		<?php
-	}
-}
 
 
 function lgw_get_post_first_trumbil_image($post_id) {
@@ -413,7 +319,5 @@ add_action( 'lgw_single_post_bottom',  'lgw_post_nav',             10 );
 add_action( 'lgw_single_post_bottom',  'lgw_display_comments',     20 );
 add_action( 'lgw_post_content_before', 'lgw_post_thumbnail',       10 );
 */
-
-
 
 require_once get_template_directory() . '/inc/wp-bootstrap-navwalker.php';
