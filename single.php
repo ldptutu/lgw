f53ea80716cc5ac558c767aec63df70c737ea296
ldp <ldp@ldp.com>
2017-12-22 11:56:49 +0800

    ..

--- single.php
+++ single.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * The template for displaying all single posts.
- *
- * @package storefront
- */
-
 get_header(); ?>
-
-
 
 <div class="row">
     <div class="col-xs-12 col-sm-6 col-md-3">
@@ -15,25 +7,21 @@ get_header(); ?>
 	    <header class="page-header">
 		<?php  the_archive_title( '<h1 class="page-title">', '</h1>' );		?>
 	    </header>
-
 	    <?php echo "abc"; ?>
 	    <?php while ( have_posts() ) : the_post();
-
 //	    do_action( 'storefront_page_before' );
 
-	    get_template_part( 'content', 'single' );
-
+//	    get_template_part( 'content', 'single' );
 //	    do_action( 'storefront_page_after' );
 
-	    endwhile; // End of the loop. ?>
-        else :
-        //	get_template_part( 'content', 'none' );
-        endif; ?>
+	    endwhile; // End of the loop.
+	    else :
+            //	get_template_part( 'content', 'none' );
+            endif; ?>
     </div>
     
     <div class="col-xs-6 col-md-9">
 	<?php get_template_part( 'template-parts/navigation/navigation', 'left' ); ?>
     </div>
 </div>
-
-<?php get_footer();
+<?php get_footer(); ?>
