35982107ffde8f2d2631579480e3678cbedbfbeb
ldp <ldp@ldp.com>
2017-12-22 14:04:38 +0800

    ...

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 get_header(); ?>
 
 <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-3">
+    <div class="col-xs-12 col-sm-6 col-md-9">
 	<?php if ( have_posts() ) : ?>
 	    <header class="page-header">
 		<?php  the_archive_title( '<h1 class="page-title">', '</h1>' );		?>
@@ -18,7 +18,7 @@ get_header(); ?>
             endif; ?>
     </div>
     
-    <div class="col-xs-6 col-md-9">
+    <div class="col-xs-6 col-md-3">
 	<?php get_template_part( 'template-parts/navigation/navigation', 'left' ); ?>
     </div>
 </div>
