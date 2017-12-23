b677b618f2598ce59fd13caf2410b9d0a3361bfe
ldp <253496582@qq.com>
2017-12-23 08:38:56 +0800

    ..

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -15,7 +15,7 @@
     <div class="widget woocommerce widget_product_search">
 	<form role="search" method="get" class="woocommerce-product-search" action="https://demo.woothemes.com/storefront/">
 	<label class="screen-reader-text" for="woocommerce-product-search-field-0">Search for:</label>
-	<input id="woocommerce-product-search-field-0" class="search-field" placeholder="Search products…" value="" name="s" type="search">
+	<input id="woocommerce-product-search-field-0" class="search-field" placeholder="Search…" value="" name="s" type="search">
 	<input value="Search" class="search-button-css" type="submit">
 	<input name="post_type" value="product" type="hidden">
     </form>
